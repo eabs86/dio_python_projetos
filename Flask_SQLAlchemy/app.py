642c200103af8e2f84717d8f35ee20184270ed7e
Emmanuel Andrade <emmanuel.andrade@gmail.com>
2023-01-04 21:37:45 -0300

    adição da parte do projeto de REST API com Flask-httpauth

--- Flask_SQLAlchemy/app.py
+++ Flask_SQLAlchemy/app.py
@@ -1,13 +1,38 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 import json
-from models import Pessoas, Atividades
+from models import Pessoas, Atividades, Usuarios
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
 
+
+# USUARIOS = {
+#     'fulano': '321',
+#     'beltrano':'321'
+# }
+
+# @auth.verify_password
+# def verificacao(login, senha):
+#     print('validando usuario...')
+#     print(USUARIOS.get(login)==senha)
+#     if not(login, senha):
+#         return False
+#     return USUARIOS.get(login) == senha
+
+@auth.verify_password
+def verificacao(login, senha):
+    print('validando usuario...')
+    if not(login, senha):
+        return False
+    return Usuarios.query.filter_by(login=login, senha=senha).first()
+    
+
 class Pessoa(Resource):
     
+    @auth.login_required
     def get(self,nome):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
@@ -48,7 +73,7 @@ class Pessoa(Resource):
         return {'status':'sucesso', 'mensagem' : mensagem}
     
 class ListaPessoas(Resource):
-    
+    @auth.login_required
     def get(self):
         pessoas = Pessoas.query.all() #só fazer isso quando a tabela tiver poucos registros. NÃO FAZER QUANDO O BANCO TEM MUITOS DADOS
         response = [{'id':i.id,'nome':i.nome,'idade':i.idade} for i in pessoas]
--- Flask_SQLAlchemy/utils.py
+++ Flask_SQLAlchemy/utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas, db_session
+from models import Pessoas, db_session,Usuarios
 
 def insere_pessoas():
     pessoa = Pessoas(nome='Beltrano', idade='21')
@@ -18,9 +18,19 @@ def exclui():
     pessoa = Pessoas.query.filter_by(nome = 'Ciclano').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
 
 if __name__ == "__main__":
+    # insere_usuario('emmanuel','1234')
+    # insere_usuario('andrade','4321')
     # insere_pessoas()
-    consulta()
+    # consulta()
     # altera()
     # exclui()
+    consulta_todos_usuarios()
