da5c3df98415dba5e093fc8b34048ef5136fbf95
Emmanuel Andrade <emmanuel.andrade@gmail.com>
2023-01-12 21:58:27 -0300

    adicionando as funcionalidades de autenticação

--- Django/agenda/core/views.py
+++ Django/agenda/core/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
 # Create your views here.
 
+
+def login_user(request):
+    return render(request,'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request,usuario)
+            return redirect('/')
+        else:
+            messages.error(request,' Usuário e/ou Senha inválidos!')
+
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     #usuario = request.user
-    evento = Evento.objects.all() #filter(usuario = usuario)#all #get(id=1)
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)#all() #filter(usuario = usuario)#all #get(id=1)
     dados = {'eventos': evento}
     return render(request,'agenda.html',dados)
+
+
 
 # def index(request):
 #     return redirect('/agenda')
